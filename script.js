22a87c17cbef6300fa05c9a58b4ca973c1bc112c
elcioveiga <elcio.veiga@escola.pr.gov.br>
2026-06-25 11:51:01 -0300

    Código pronto

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function revelarMensagem() {
 	document.getElementById("mensagem").innerText = "Olá, mundo! Aprendendo JS";
 }
 
+function mostraNome() {
+	let nome = document.getElementById("nomeUsuario").value;
+
+	document.getElementById("resultadoNome").innerText = "Bem-vindo, " + nome + "!"
+}
